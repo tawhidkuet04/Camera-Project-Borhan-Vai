1a53878450bc8d186f55cff2abcbf9358d5b514f
Tawhid Joarder <tawhid@Sks-MacBook-Pro.local>
2019-04-16 13:09:42 +0600

    updated slider esay

--- CameraProject/PlayerDisplayVCViewController.m
+++ CameraProject/PlayerDisplayVCViewController.m
@@ -12,7 +12,6 @@
     AVPlayer *player;
     IBOutlet UIButton *playPause;
     IBOutlet UISlider *slider;
-    __weak NSObject *weakSelf;
     AVPlayerItem *playerItem ;
    
 }
@@ -29,7 +28,6 @@
     slider.maximumValue = CMTimeGetSeconds(playerItem.asset.duration);
     slider.hidden =  NO;
     [slider setValue:0];
-    weakSelf = self;
    // __weak NSObject *weakSelf = self;
    
 
@@ -47,6 +45,7 @@
     // _playerView.player = player;
     [self.playerView setPlayer:player];
 }
+#pragma  mark -play/pause
 - (IBAction)playPauseAction:(id)sender {
     UIButton *btn = (UIButton*)sender;
     if (player.rate) {
@@ -56,20 +55,8 @@
     }else{
        
         [btn setTitle:@"Pause" forState:UIControlStateNormal];
-        double interval = .1f;
         
-        CMTime playerDuration = [self playerItemDuration]; // return player duration.
-        if (CMTIME_IS_INVALID(playerDuration))
-        {
-            return;
-        }
-        double duration = CMTimeGetSeconds(playerDuration);
-        if (isfinite(duration))
-        {
-            CGFloat width = CGRectGetWidth([slider bounds]);
-            interval = 0.5f * duration / width;
-        }
-        [player addPeriodicTimeObserverForInterval:CMTimeMakeWithSeconds(1/ 60.0, NSEC_PER_SEC)
+        [player addPeriodicTimeObserverForInterval:CMTimeMakeWithSeconds(1/ 10000.0, NSEC_PER_SEC)
                                              queue:NULL
                                         usingBlock:^(CMTime time){
                                             [self updateSlider];
@@ -85,9 +72,18 @@
     [self.timer invalidate];
     [slider setValue:0];
 }
+#pragma mark -slider
 - (IBAction)sliderValueChanged:(UISlider *)sender {
     [player seekToTime:CMTimeMakeWithSeconds(sender.value, NSEC_PER_SEC) toleranceBefore:kCMTimeZero toleranceAfter:kCMTimeZero];
 }
+- (void)updateSlider {
+    
+    double time = CMTimeGetSeconds([player currentTime]);
+    // NSLog(@"min %f max %f time %f dur %f",minValue,maxValue,time,duration);
+    [slider setValue:time];
+    
+}
+#pragma  mark -back
 - (IBAction)backButtonPressed:(id)sender {
     player = nil;
     playerItem = nil;
@@ -96,41 +92,7 @@
     [self.navigationController popViewControllerAnimated:YES];
    // [self presentViewController:cameraWindow  animated:YES completion:nil];
 }
-- (void)updateSlider {
-    CMTime playerDuration = [self playerItemDuration];
-    if (CMTIME_IS_INVALID(playerDuration))
-    {
-        slider.minimumValue = 0.0;
-        return;
-    }
-    
-    double duration = CMTimeGetSeconds(playerDuration);
-    if (isfinite(duration) && (duration > 0))
-    {
-        float minValue = [ slider minimumValue];
-        float maxValue = [ slider maximumValue];
-        double time = CMTimeGetSeconds([player currentTime]);
-        [slider setValue:(maxValue - minValue) * time / duration + minValue];
-    }
-}
-- (CMTime)playerItemDuration
-{
-    AVPlayerItem *thePlayerItem = [player currentItem];
-    if (thePlayerItem.status == AVPlayerItemStatusReadyToPlay)
-    {
-        
-        return([playerItem duration]);
-    }
-    
-    return(kCMTimeInvalid);
-}/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
